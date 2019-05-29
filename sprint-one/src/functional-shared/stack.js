f609cab3c846d70340097ebe03e841bd2bfa9b5f
Kirk Shin <kirkshin@Kirks-MacBook-Air.local>
2019-05-29 16:27:53 -0700

    3rd edit fun-share stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,44 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  newStack.nextIndex = 0;
+  extend(newStack, stackMethods);
+
+
+  return newStack;
 };
 
-var stackMethods = {};
+var extend = function(to, from) {
+	for(var key in from) {
+		to[key] = from[key]; 
+	}
+};
+
+stackMethods = {};
+
+//stackMethods.nextIndex = 0;
+
+stackMethods.push = function(value) {		
+	this.storage.nextIndex = value;
+	this.nextIndex += 1;
+}
+
+stackMethods.pop = function() {	
+	if(this.nextIndex === 0) {
+		this.nextIndex = 0;
+		return this.storage.nextIndex;
+	}
+	this.nextIndex--;
+	var toReturn = this.storage.nextIndex;
+	delete this.storage.nextIndex;
+	return toReturn;
+	
+}
+
+stackMethods.size = function() {
+	return this.nextIndex;
+}
 
 
