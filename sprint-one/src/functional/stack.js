fde2dc203251daee33f4a09ccd7fea5ecc640a60
Kirk Shin <kirkshin@Kirks-MacBook-Air.local>
2019-05-29 12:28:38 -0700

    first edit, fun stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,30 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  
+
   someInstance.push = function(value) {
+    var entriesArr = Object.entries(storage); //[[key, value]]
+    var index = entriesArr.length;
+    /*
+    if(entriesArr.length === 0) {
+      index = 0;
+    } else {
+      index = entriesArr.length;  //last index
+    }
+    */
+    storage[index] = value;
   };
 
   someInstance.pop = function() {
+    var entriesArr = Object.entries(storage);
+    var lastVal = Object.values(storage)[entriesArr.length - 1];
+    delete storage[entriesArr.length - 1];
+    return lastVal;
   };
 
   someInstance.size = function() {
+    return Object.entries(storage).length;
   };
 
   return someInstance;
