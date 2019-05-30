60574f929dbed572a6df868c03614251ca981e71
Kirk Shin <kirkshin@Kirks-MacBook-Air.local>
2019-05-30 13:34:29 -0700

    1st edit linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,28 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = Node();
+  list.tail = Node();
 
   list.addToTail = function(value) {
+    if(Object.values(this).length === 0) {
+      this.head.value = value;
+    } else {
+      this.tail.next = value; //pair old tail's next to incoming value
+      
+      this.value = Node(value);
+    }
   };
 
   list.removeHead = function() {
+    var newHead = this.head.next;
+    delete this.head;
+    this.head = newHead;
+    return newHead;
   };
 
   list.contains = function(target) {
+    return this.value !== undefined || this.head.value !== undefined || this.tail.value !== undefined;
+
   };
 
   return list;
