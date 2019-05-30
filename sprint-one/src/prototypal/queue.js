46452dfc7e308513b886f9d705804a746663de36
Kirk Shin <kirkshin@Kirks-MacBook-Air.local>
2019-05-30 09:54:26 -0700

    2nd edit proto queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.nextIndex = 0;
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+	this[this.nextIndex] = value;
+	this.nextIndex++;
+};
+
+queueMethods.dequeue = function() {
+	if(this.nextIndex === 0) {
+		return;
+	}
+	this.nextIndex--;
+	var toReturn = this[0];
+	var copyQueue = {};
+	//copy current data into copyQueue
+	for(var key in this) {
+		copyQueue[key] = this[key];
+	}
+	//shift newQueue using copyQueue as template
+	for(var i = 0; i < this.nextIndex; i++) {
+		this[i] = copyQueue[i + 1];
+	}
+	delete this[this.nextIndex];
+	return toReturn;
+
+};
+
+queueMethods.size = function() {
+	return this.nextIndex;
+}
 
