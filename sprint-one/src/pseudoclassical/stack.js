1928cd6a9553fea1981ce742251c18f74c819bf8
Kirk Shin <kirkshin@Kirks-MacBook-Air.local>
2019-05-30 10:54:05 -0700

    1st edit pseudo stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.nextIndex = 0;
 };
 
+Stack.prototype.push = function(value) {
+	this[this.nextIndex] = value;
+	this.nextIndex++;
+};
 
+Stack.prototype.pop = function() {
+	if(this.nextIndex === 0) {
+		return;
+	}
+	this.nextIndex--;
+	var toReturn = this[this.nextIndex];
+	delete this[this.nextIndex];
+	return toReturn;
+};
+
+Stack.prototype.size = function() {
+	return this.nextIndex;
+}
+
+var stack = new Stack();
