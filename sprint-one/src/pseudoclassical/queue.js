b456eded72ea64b047f65747374eef4d09a39f51
Kirk Shin <kirkshin@Kirks-MacBook-Air.local>
2019-05-30 11:04:30 -0700

    1st edit pseudo queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.nextIndex = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+	this[this.nextIndex] = value;
+	this.nextIndex++;
+};
 
+Queue.prototype.dequeue = function() {
+	if(this.nextIndex === 0) {
+		return;
+	}
+	this.nextIndex--;
+	var toReturn = this[0];
+	//copy queue into copyQueue to use as template to modify orig queue
+	var copyQueue = {};
+	for(var key in this) {
+		copyQueue[key] = this[key];
+	}
+	for(var i = 0; i < this.nextIndex; i++) {
+		this[i] = copyQueue[i + 1];
+	}
+	delete this[this.nextIndex];
+	return toReturn;
+};
+
+Queue.prototype.size = function() {
+	return this.nextIndex;
+};
+
+var queue = new Queue();
